8ad03de2a255ad028778648492015cb9192e82d2
Julia Vu <juliadvu@gmail.com>
2020-05-21 16:56:19 -0500

    made updates to migrations file

---
+++ data/migrations/20200521165210_food.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex) {
+    return(
+        knex.schema
+          .createTable('foods', tbl=>{
+              tbl.increments(); //gives unique id
+              tbl.string('name', 255).notNullable();
+              tbl.string('category', 255).notNullable();
+  
+          })
+    )
+  };
+  
+  exports.down = function(knex) {
+      return knex.schema.dropTableIfExists("foods");
+  };
+  
